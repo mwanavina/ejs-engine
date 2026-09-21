25f8f02c271dc951da7068308229ecc6e43c33af
Mark Mwanavina <mmwanavina@gmail.com>
2026-09-21 09:25:19 +0200

    Add navbar component and update index view to render links

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const links =[
+    { href: "/", text: "Home" },
+    { href: "/about", text: "About" },
+    { href: "/contact", text: "Contact" }
+];
 app.get("/",(req, res) => {
-    res.render("index", { message: "EJS rocks!" });
+    res.render("index", { links: links    });
 });
 
 app.listen(3000, () => {
