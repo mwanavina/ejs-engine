41fbbcb9937f59e0fd41b1826c91e35ea25864fa
Mark Mwanavina <mmwanavina@gmail.com>
2026-09-21 09:55:54 +0200

    Add user data to index route and improve route handling

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 const links =[
     { href: "/", text: "Home" },
     { href: "/about", text: "About" },
     { href: "/contact", text: "Contact" }
 ];
+
+const users = ["Alice", "Bob", "Charlie"];
 app.get("/",(req, res) => {
-    res.render("index", { links: links    });
+    res.render("index", { links: links, users: users});
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { links: links });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { links: links });
 });
 
 app.listen(3000, () => {
